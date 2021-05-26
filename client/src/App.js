c10c1ae3c3fbeee8e87740e9c4270b0a26990577
dkreth-op <daniel.kreth@objective-partner.com>
2021-05-26 16:37:50 +0200

    aws multi container deployment test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Multi Container AWS
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
